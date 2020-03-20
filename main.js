aa12a905ed3a79e4a28557fb0c6665824c7bc46e
serhiy-kostiuk <serhiy.kostiuk@selectoglobal.com>
2020-03-20 18:58:05 +0200

    2

--- main.js
+++ main.js
@@ -1,16 +1,52 @@
 window.onload = () => {
   /**
-   * @type {Element}
+   * @returns {Element}
    */
-  const el = document.getElementById('root');
+  const p = success => {
+    let color;
+    switch (success) {
+      case true:
+        color = 'darkgreen';
+        break;
+      case false:
+        color = 'tomato';
+        break;
+      default:
+        color = 'black';
+    }
 
-  const MD = navigator.mediaDevices;
-  const MDuserMedia = MD && MD.getUserMedia;
-  const NuserMedia = navigator.getUserMedia
+    const el = document.createElement('p');
+    el.style.color = color;
+    document.body.appendChild(el);
+    return el;
+  }
 
-  el.innerHTML = `
-    navigator.mediaDevices: ${typeof MD} <br />
-    navigator.mediaDevices.getUserMedia: ${typeof MDuserMedia} <br />
-    navigator.getUserMedia: ${typeof NuserMedia}
-    `
+  const devMedia = typeof (navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+  const navMedia = typeof (navigator.getUserMedia);
+
+  p().innerHTML = `navigator.mediaDevices.getUserMedia: ${devMedia} <br /> navigator.getUserMedia: ${navMedia}`;
+
+  if (devMedia === 'function') {
+    navigator.mediaDevices.getUserMedia({ audio: true })
+      .then(() => p(true).innerHTML = 'navigator.mediaDevices.getUserMedia -> AUDIO SUCCESS')
+      .catch(({ name }) => p(false).innerHTML = 'navigator.mediaDevices.getUserMedia -> AUDIO ' + name);
+
+    navigator.mediaDevices.getUserMedia({ video: true })
+      .then(() => p(true).innerHTML = 'navigator.mediaDevices.getUserMedia -> VIDEO SUCCESS')
+      .catch(({ name }) => p(false).innerHTML = 'navigator.mediaDevices.getUserMedia -> VIDEO ' + name);
+  }
+
+  if (navMedia === 'function') {
+    const wrapper = options => {
+      return new Promise((resolve, reject) => navigator.getUserMedia(options, resolve, reject));
+    };
+
+    wrapper({ audio: true })
+      .then(() => p(true).innerHTML = 'navigator.getUserMedia -> AUDIO SUCCESS')
+      .catch(({ name }) => p(false).innerHTML = 'navigator.getUserMedia -> AUDIO ' + name);
+
+    wrapper({ video: true })
+      .then(() => p(true).innerHTML = 'navigator.getUserMedia -> VIDEO SUCCESS')
+      .catch(({ name }) => p(false).innerHTML = 'navigator.getUserMedia -> VIDEO ' + name);
+  }
 }
